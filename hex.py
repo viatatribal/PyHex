9704de4ae2103bcf311d6c15dd5db4135bd39dd2
urekmazino <shlover.v@gmail.com>
2023-08-08 17:18:20 -0300

    Write stub for main functions

--- hex.py
+++ hex.py
@@ -7,8 +7,10 @@ graph = {
     'p2' : []
 }
 
+# size of board
+N = 2
 # The board game is represented by a matrix nxn
-board = [[0 for i in range(2)] for i in range(2)]
+board = [[0 for i in range(N)] for i in range(N)]
 
 # dfs function
 visited = set()
@@ -19,11 +21,28 @@ def dfs(visited, graph, node):
         for neighbour in graph[node]:
             dfs(visited, graph, neighbour)
 
+#  add nodes to the graph
+def addNodes():
+    pass
+
+
+# generate all neighbour of a given position
+def genNeighbour(pos):
+    pass
+
+
+# check the neighbour of a position
+# so we can add it to the graph
+def checkNeighbour(x,y, neighbour):
+    for n in neighbour:
+        pass
+
+
 # set position in board
 def setPosition(x,y, player):
-    board[x][y] = x + y
-    graph[player].append(x+y)
-    if x+y not in graph:
-        graph[x+y] = []
-
+    pos = x*N + y + 1
+    board[x][y] = pos
+    graph[player].append(pos)
+    if pos not in graph:
+        graph[pos] = []
 
